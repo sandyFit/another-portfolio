661d76ec18ec6f13941b18efd65f8b9394c9f2b2
SandyFit <sandy07r@gmail.com>
2024-03-30 22:18:26 -0500

    style adjustments

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -38,12 +38,12 @@ const Footer = () => {
     
 
     return (
-        <section id='contact' className='w-full h-screen bg-slate-950 flex flex-col'>
+        <section id='contact' className='w-full h-screen bg-slate-950 flex flex-col relative'>
             {/* Content */}
 
              <div className="flex justify-between w-full relative">
                 <div className="w-[9%] flex justify-start items-center font-ibm text-lg font-[400] text-purple-500 
-                    ml-24 mb-10 relative top-36">                    
+                    ml-24 mb-10 relative top-36 left-8">                    
                     <span className='text-electro-300 mr-3'>{'<'}</span>
                     <h3 className='w-full tracking-widest'>say hi</h3> 
                     <span><BsEmojiSmile /></span>
@@ -67,45 +67,44 @@ const Footer = () => {
              
 
             <div className="flex flex-col justify-between items-start w-full h-full mt-40">                  
-                <div className="flex flex-col w-full justify-between items-center h-full relative">
+                <div className="flex flex-col w-full justify-between items-start h-full relative ml-48">
                     <div className="flex justify-start gap-2">
-                        <FaCircle
-                            style={{ color: '#2ef938', fontSize: '1.2rem', margin: '15px 10px 0 0', zIndex: '1' }} />
+                        {/* <FaCircle
+                            style={{ color: '#2ef938', fontSize: '1.2rem', margin: '35px 10px 0 0', zIndex: '1' }} /> */}
                         <h1 className='font-ibm uppercase text-[3.65rem] text-purple-500 font-[500] 
-                                tracking-wide leading-[3.5rem]'>
+                                tracking-widest'>
                             Eager to work together?
                         </h1>
                     </div>
                     
-                    <p className='w-[52%] font-ibm text-zinc-400 font-[400] tracking-[.1rem] mt-10 ml-12
+                    <p className='w-[52%] font-ibm text-zinc-400 font-[400] tracking-[.1rem] mt-4 
                         text-[1.05rem] relative'>
-                        Cool stuff takes time & your first step is just an email away. <br/>
-                        Fancy a chat about web wonders? Simply drop me a line at  
+                        {/* Cool stuff takes time & your first step is just an email away. <br/> */}
+                        Let's chat about web wonders. Just drop me a line at  
                         <span className='lowercase text-electro-300 ml-2'>hello@trish.com</span>.<br/>
                         The more specific you are, the better! I'll circle back to you within 1-3 business days,
                         loaded with fresh ideas and perhaps a pun or two.<br/>
                         Thanks for stopping by!
                     </p>
 
-                    <p className='text-[14rem] font-[500] text-[#14062e] tracking-tighter absolute top-28
-                        leading-loose'>
-                        hello@trish.com
-                    </p>
+                    
                 
-                    <div className="flex justify-start items-center mt-[8rem] gap-32 z-20">
-                        <button className="flex h-full items-end justify-start font-ibm text-lg font-[400] 
-                        text-electro-300 border-[1px] border-dotted border-purple-900 py-1 px-10 uppercase">                                                
-                            <a href='mailto:sandy07r@gmail.com' className='w-full tracking-widest'>Send Mail</a>                                                                                                
-                        </button>
+                    <div className="flex mt-[2.5rem] ">
+                        <div className="flex justify-start items-start gap-12 z-20">
+                            <button className="flex h-full items-end justify-start font-ibm text-lg font-[400] 
+                            text-electro-300 border-[1px] border-dotted border-purple-400 px-12 py-2 uppercase">                                                
+                                <a href='mailto:sandy07r@gmail.com' className='w-full tracking-widest'>Send Mail</a>                                                                                                
+                            </button>
 
-                         <button className="flex h-full items-end justify-start font-ibm text-lg font-[400]  
-                            text-electro-300 border-[1px] border-dotted border-purple-900 py-1 px-10 uppercase">                                              
-                            <h3 className='w-full tracking-widest'>Copy Mail </h3>                                                                                                     
-                        </button>
+                            <button className="flex h-full items-end justify-start font-ibm text-lg font-[400]  
+                                text-electro-300 border-[1px] border-dotted border-purple-400 px-12 py-2 uppercase">                                              
+                                <h3 className='w-full tracking-widest'>Copy Mail </h3>                                                                                                     
+                            </button>
+                        </div>
                     </div>
                 </div>               
                 
-                <div className="flex justify-end items-end w-[9.6%] ml-24 pb-16 relative left-[99.5rem] top-[10rem]">
+                <div className="flex justify-end items-end w-[9.6%] ml-24 pb-16 relative left-[99.5rem] top-[17rem]">
                     <ScrollLink to="hero"
                         smooth={true} 
                         duration={500}
@@ -119,7 +118,7 @@ const Footer = () => {
                 </div> 
                
                 
-                    <div className="w-full h-full flex flex-col mt-[8rem] pl-32">                        
+                    <div className="w-full h-full flex flex-col mt-[14rem] pl-32">                        
                         <hr className='w-[94%] border-t-[1px] border-dotted border-purple-400 ' />
                         <div className="w-full flex justify-between">
                             <div className="w-full h-full flex font-ibm uppercase text-zinc-400 mt-5
@@ -132,7 +131,7 @@ const Footer = () => {
                                 {'by yours truly'}
                             </div>   
                         
-                            <p className='w-full h-full flex font-ibm uppercase text-zinc-400 mt-5 pl-[37rem]
+                            <p className='w-full h-full flex font-ibm uppercase text-zinc-400 mt-5 pl-[36rem]
                                 tracking-wide'>
                                 Be kind, Do Good & create web wonders.
                             </p>
@@ -140,7 +139,10 @@ const Footer = () => {
                     </div>
                 </div>
 
-                
+                <p className='text-[15rem] font-[500] text-[#110921] tracking-tight absolute top-[28.5rem]
+                    left-28 leading-loose'>
+                    hello@trish.com
+                </p>
                                  
         </section>
     )
